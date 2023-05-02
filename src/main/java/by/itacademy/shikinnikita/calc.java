2fcfd2478a23cb158052b50c6fd6bf5d94d53664
Mikita Shykin <mikita.shikin@gmail.com>
2023-05-02 18:54:37 +0200

    add method substraction

--- src/main/java/by/itacademy/shikinnikita/calc.java
+++ src/main/java/by/itacademy/shikinnikita/calc.java
@@ -1,8 +1,11 @@
 package by.itacademy.shikinnikita;
 
 public class calc {
-    public static int sum(int a, int b, int c){
-        return a+b;
+    public static int sum(int a, int b) {
+        return a + b;
+    }
 
+    public static int substract(int a, int b) {
+        return a - b;
     }
 }
