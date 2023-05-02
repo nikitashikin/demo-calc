dc3cbea329874ae9407557281c06c621f149a020
Mikita Shykin <mikita.shikin@gmail.com>
2023-05-02 18:56:27 +0200

    add method divide

--- src/main/java/by/itacademy/shikinnikita/calc.java
+++ src/main/java/by/itacademy/shikinnikita/calc.java
@@ -8,9 +8,12 @@ public class calc {
     public static int substract(int a, int b) {
         return a - b;
     }
+
     public static int multiplicate(int a, int b) {
         return a * b;
     }
+
     public static int divide(int a, int b) {
         return a / b;
+    }
 }
